132f24c648f0ae0c0bd8f787cf7ef77c7e9a4f80
Kasper <128734476+Artoniks@users.noreply.github.com>
2023-11-16 17:09:21 +0200

    Add files via upload

---
+++ js/game-dino.js
@@ -0,0 +1,68 @@
+alert("Ви готові?");
+
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const btn = document.getElementById('btn');
+const diff = document.getElementById('difficult');
+
+
+btn.addEventListener('click', function(event) {
+    btn.value == "Stop" ? stop() : isAlive = gameStart();
+    this.blur();
+  });
+
+
+ function gameStart () {
+    btn.value = "Stop";
+    btn.style.background = "green";
+    cactus.style.animationPlayState = 'running'; 
+  
+  }
+
+
+document.addEventListener("keydown", function (event) {
+    jump();
+});
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
+    }
+    setTimeout(function () {
+        dino.classList.remove("jump");
+    }, 425);
+}
+
+
+function checkCollision() {
+    let dinoRect = dino.getBoundingClientRect();
+    let cactusRect = cactus.getBoundingClientRect();
+
+    if (
+        dinoRect.bottom >= cactusRect.top &&
+        dinoRect.right >= cactusRect.left &&
+        dinoRect.left <= cactusRect.right
+    ) {
+        clearInterval(gameInterval);
+        alert("Game Over!");
+        window.location.reload();
+    }
+}
+
+checkCollision();
+
+let gameInterval = setInterval(function () {
+    checkCollision();
+}, 10);
+
+function stop(){
+    btn.value = "Start";
+    btn.style.background = "red";
+    cactus.style.animationPlayState = 'paused';
+  }
+ 
+
+function changeSpeed(){
+    let idx = difficult.options.selectedIndex;
+   
+}
